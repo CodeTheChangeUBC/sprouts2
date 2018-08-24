9b0a25a0f9608626880e388fce1ce04b32171880
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2018-08-23 19:19:10 -0700

    Added default auth with new user pool

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator} from 'aws-amplify-react';
+import {Auth} from 'aws-amplify';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       return(
+          
+            <button onClick={this.props.OAuthSignIn}>
+                Sign in with AWS
+            </button>
     );
   }
 }
 
-export default App;
+
+export default withAuthenticator(App);
