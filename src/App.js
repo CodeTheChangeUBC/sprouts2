b033d8a7fe72c0ef711e7d194c160275ef48a219
vinielk <vinielk@icloud.com>
2018-08-26 13:56:27 -0700

    added react router and started routing for admin panel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,88 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import { ConfirmSignIn, ConfirmSignUp, ForgotPassword, SignUp, VerifyContact, withAuthenticator } from 'aws-amplify-react';
 import { MySignIn } from './components/authentication/MySignIn';
+import { Auth } from 'aws-amplify';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
 
 class App extends Component {
   render() {
+    let userGroup;
+    Auth.currentSession()
+      .then(session => userGroup = session.accessToken.payload['cognito:groups'][0])
+      .catch(err => console.log(err));
     return(
       <div>
-        <h1>Sprouts Application</h1>
+        <BasicExample />
       </div>
     );
   }
 }
-
-
 
 export default withAuthenticator(App, {includeGreetings:true}, [
   <MySignIn/>,
