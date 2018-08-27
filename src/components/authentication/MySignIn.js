50d5f70e0bbd640499c3df3d6792e539c05b3ef2
vinielk <vinielk@icloud.com>
2018-08-26 20:03:51 -0700

    styled sign in page

--- src/components/authentication/MySignIn.js
+++ src/components/authentication/MySignIn.js
@@ -10,21 +10,34 @@ export class MySignIn extends SignIn {
     const hideForgotPassword = hide.some(component => component.name === 'ForgotPassword')
 
     return (
-      <div>
-        <input type="text" id="username" key="username" name="username" placeholder="Username" onChange={this.handleInputChange}/>
-        <input type="password" id="password" key="password" name="password" placeholder="Password" onChange={this.handleInputChange}/>
-        <button onClick={ this.signIn } value="Sing In" >{I18n.get('Sign In')}</button>
-        <div>
-          { !hideForgotPassword && 
-            <a onClick={() => this.changeState('forgotPassword')}>
-              {I18n.get('Forgot Password')}
-            </a> }
-        </div>
-        <div>
-          { !hideSignUp && 
-            <a onClick={() => this.changeState('signUp')}>
-              {I18n.get('Sign Up')}
-            </a> }
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center my-3">
+            <h1>Sign In</h1>
+            <div className="form-group">
+              <input className="form-control" type="text" id="username" key="username" name="username" placeholder="Username" onChange={this.handleInputChange}/>
+            </div>
+            <div className="form-group">
+              <input className="form-control" type="password" id="password" key="password" name="password" placeholder="Password" onChange={this.handleInputChange}/>
+            </div>
+            <div className="row justify-content-center py-1">
+              <div className="col-12 col-md-">
+                <button className="btn btn-block rounded-0 btn-primary" onClick={ this.signIn } value="Sign In" >{I18n.get('Sign In')}</button>
+              </div>
+            </div>
+            <div className="py-1">
+              { !hideSignUp && 
+                <a className="btn btn-block rounded-0 btn-primary text-white" onClick={() => this.changeState('signUp')}>
+                  {I18n.get('Sign Up')}
+                </a> }
+            </div>
+            <div className="py-1">
+              { !hideForgotPassword && 
+                <a href="#" onClick={() => this.changeState('forgotPassword')}>
+                  {I18n.get('Forgot Password')}
+                </a> }
+            </div>
+          </div>
         </div>
       </div>
     )
