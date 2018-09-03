ba03473cc48714b4c490a563350c160e74c9a3d9
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2018-09-02 18:06:02 -0700

    Partial functionality

--- src/components/admin_panel/ShiftDetails.js
+++ src/components/admin_panel/ShiftDetails.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import { Header } from '../fsc/Header';
 import { Input } from '../fsc/Input';
+import { Button } from '../fsc/Button';
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 
-export const ShiftDetails = (props) => {
-  return (
-    <div>
-      <Header title="Shift Details" link={() => props.history.push('/viewLogs')}/>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 my-3">
-            <Input value={props.name} title="User" disabled={true}/>
-            <Input value={props.date} title="Date" type="date" disabled={true}/>
-            <Input value={props.startTime} title="Start time" type="time" disabled={true}/>
-            <Input value={props.endTime} title="End time" type="time" disabled={true}/>
-            <Input value={props.meal} title="Meal" disabled={true}/>
-            <Input value={`${props.cost}`} title="Cost ($)" type="number" disabled={true}/>
+export class ShiftDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      name: this.props.name,
+      date: this.props.date,
+      startTime: this.props.startTime,
+      endTime: this.props.endTime,
+      meal: this.props.meal,
+      cost: this.props.cost,
+      edit: false,
+    };
+    this.edit = this.edit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  edit(event) {
+    this.setState({edit: !this.state.edit});
+    alert(this.props.name + " " + this.props.date);
+  }
+
+  handleSubmit(event) {
+    alert(this.state.name + " " + this.state.meal);
+  }
+
+
+  render = (props) => {
+    return (
+      <div>
+        <Header title="Shift Details" link={() => this.props.history.push('/viewLogs')}/>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 my-3">
+              <Input value={this.state.name} title="User" update={(event) => this.setState({name: event.target.value})} disabled={(this.state.edit)? "" : "disabled"}/>
+              <Input value={this.state.date} title="Date" type="date" disabled={(this.state.edit)? "" : "disabled"}/>
+              <Input value={this.state.startTime} title="Start time" type="time" disabled={(this.state.edit)? "" : "disabled"}/>
+              <Input value={this.state.endTime} title="End time" type="time" disabled={(this.state.edit)? "" : "disabled"}/>
+              <Input value={this.state.meal} title="Meal" disabled={(this.state.edit)? "" : "disabled"}/>
+              <Input value={`${this.state.cost}`} title="Cost ($)" type="number" disabled={(this.state.edit)? "" : "disabled"}/>
+              <Button title="Edit" color="btn-primary rounded-0 btn-block" onClick={this.edit}/>
+              <Button title="Save" color="btn-secondary rounded-0 btn-block" onClick={this.handleSubmit} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 ShiftDetails.propTypes = {
   name: PropTypes.string,
@@ -29,7 +60,6 @@ ShiftDetails.propTypes = {
   startTime: PropTypes.string,
   endTime: PropTypes.string,
   meal: PropTypes.string,
-  cost: PropTypes.number
-};
+  cost: PropTypes.number};
 
-export default ShiftDetails;
+export default withRouter(ShiftDetails);
