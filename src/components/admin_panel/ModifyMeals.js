1c4a571703ebbbabc6083acf70f6e86efe6884f1
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2018-10-24 09:46:43 -0700

    minor refactoring

--- src/components/admin_panel/ModifyMeals.js
+++ src/components/admin_panel/ModifyMeals.js
@@ -40,48 +40,53 @@ export class ModifyMeals extends React.Component {
     );
     this.setState({tableData: tableData});
   }
-  
-  saveMealOption() {
-      let apiName = 'Meal_OptionsCRUD';
-      let path = '/Meal_Options';
-      let init = {
-        body: {
-          Name: this.state.newItem,
-          Price: this.state.newPrice
-        }
-      }
-      API.put(apiName, path, init).then(response => {
-        console.log(response);
-        this.selectedRow = false;
-        this.getTableData();
-        this.setState({newItem: "", newPrice: -1});
-      }).catch(error => {
-        console.log(error);
-      });
-  }
-  
+
   addMealOption() {
-    let valid = true;
+    let canEdit = true;
     if (!this.state.edit) {
       this.setState({edit: !this.state.edit});
-      valid = false;
+      canEdit = false;
     }
-    if (this.state.edit) {
-      if (this.state.newItem === "") {
-        alert("Please enter the name of your new item"); 
-        valid = false;
-      } else if (this.state.newPrice === -1 || 
-        this.state.newPrice === "") {
-        alert("Please enter the price of your new item");
-        valid = false;
-      }
-      else if (valid) {
-        this.saveMealOption();
-        console.log(this.state.newItem + "PRICE : " + this.state.newPrice);
-        this.setState({edit: !this.state.edit});
-      }
+    if (this.state.edit && this.isValidandComplete() && canEdit) {
+      this.saveMealOption();
+      console.log(this.state.newItem + "PRICE : " + this.state.newPrice);
+      this.setState({edit: !this.state.edit});
     }
   }
+  isValidandComplete() {
+    let valid = true;
+    if (this.state.newItem === "") {
+      alert("Please enter the name of your new item."); 
+      valid = false;
+    } else if (this.state.newPrice === -1 || 
+      this.state.newPrice === "") {
+      alert("Please enter the price of your new item.");
+      valid = false;
+    } else if (this.state.newPrice < 0) {
+      alert("Please enter a valid price for your new item.");
+      valid = false;
+    }
+    return valid;
+  }
+  saveMealOption() {
+    let apiName = 'Meal_OptionsCRUD';
+    let path = '/Meal_Options';
+    let init = {
+      body: {
+        Name: this.state.newItem,
+        Price: this.state.newPrice
+      }
+    }
+    API.put(apiName, path, init).then(response => {
+      console.log(response);
+      this.selectedRow = false;
+      this.getTableData();
+      this.setState({newItem: "", newPrice: -1});
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
   deleteMealOption() {
     let apiName = 'Meal_OptionsCRUD';
     let path = '/Meal_Options/object';
@@ -90,7 +95,7 @@ export class ModifyMeals extends React.Component {
       console.log(response); 
       this.selectedRow = false;
       this.getTableData();
-      this.setState({newItem: "", newPrice: -1});
+      this.setState({newItem: "", newPrice: -1, edit: false});
     }).catch(error => {
       console.log("Err " + error);
     });
@@ -101,6 +106,7 @@ export class ModifyMeals extends React.Component {
     this.selectedRow = true;
     this.setState({newItem: row.Name ,newPrice: row.Price,edit: true})
   }
+
   render() {
     return (
       <div>
@@ -111,29 +117,30 @@ export class ModifyMeals extends React.Component {
               <div className="pb-5 mb-4">
                 <Table col1="Meal" col2="Price" select={this.selectRow} data={this.state.tableData}/>
                 <div hidden={!this.state.edit}>
-                <Input title="Meal Option" placeholder="Ex. Sandwich" disabled={this.selectedRow} value={this.state.newItem} type="string" 
-                  update={(event) => this.setState({newItem: event.target.value})}/>
-                <Input title="Price" placeholder="Ex. 7" value={(this.state.newPrice===-1)? "" : this.state.newPrice} type="number" 
-                  update={(event) => this.setState({newPrice: event.target.value})}/>
-                <div hidden={!this.selectedRow}>
-                  <Button title="Update" color="btn-primary rounded-0 btn-block" 
-                    onClick={() => this.addMealOption()} />
-                  <Button title="Delete" disabled={this.state.newItem === "" || this.state.newPrice === ""}
-                    color="btn-danger rounded-0 btn-block" onClick={() => this.deleteMealOption()} />
-                  <Button title="Cancel" disabled={!this.state.edit} color="btn-secondary rounded-0 btn-block" 
-                    onClick={() => {this.selectedRow = false;
-                        this.setState({edit: false, newItem: "", newPrice: ""})}} />
-
+                  <Input title="Meal Option" placeholder="Ex. Sandwich" disabled={this.selectedRow} 
+                    value={this.state.newItem} type="string" 
+                    update={(event) => this.setState({newItem: event.target.value})}/>
+                  <Input title="Price" placeholder="Ex. 7" 
+                    value={(this.state.newPrice===-1)? "" : this.state.newPrice} type="number" 
+                    update={(event) => this.setState({newPrice: event.target.value})}/>
+                  <div hidden={!this.selectedRow}>
+                    <Button title="Update" color="btn-primary rounded-0 btn-block" 
+                      onClick={() => this.addMealOption()} />
+                    <Button title="Delete" disabled={this.state.newItem === "" || this.state.newPrice === ""}
+                      color="btn-danger rounded-0 btn-block" onClick={() => this.deleteMealOption()} />
+                    <Button title="Cancel" disabled={!this.state.edit} color="btn-secondary rounded-0 btn-block" 
+                      onClick={() => {
+                        this.selectedRow = false;
+                        this.setState({edit: false, newItem: "", newPrice: ""})
+                      }} />
+                  </div>
                 </div>
-              </div>
-              <div hidden={this.selectedRow}>
-              <Button title={this.state.edit ? "Add" : "Add New Meal Option"} color="btn-primary rounded-0 btn-block" 
-                    onClick={() => this.addMealOption()} />
-              <Button title="Cancel" disabled={!this.state.edit} color="btn-secondary rounded-0 btn-block" 
-                    onClick={() => this.setState({edit: false, newItem: "", newPrice: ""})} />
-              </div>
-              </div>
-              <div className="container bg-white py-1 fixed-bottom">
+                <div hidden={this.selectedRow}>
+                <Button title={this.state.edit ? "Add" : "Add New Meal Option"} color="btn-primary rounded-0 btn-block" 
+                      onClick={() => this.addMealOption()} />
+                <Button title="Cancel" disabled={!this.state.edit} color="btn-secondary rounded-0 btn-block" 
+                      onClick={() => this.setState({edit: false, newItem: "", newPrice: ""})} />
+                </div>
               </div>
             </div>
           </div>
